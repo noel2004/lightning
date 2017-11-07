decd5c54feabfc83b1d11b17c589c4fbb202cc45
Linfeng Liang (梁林峰) <lianglinfeng@le.com>
2017-11-07 10:11:46 +0800

    u

--- scripts/build-windows-mingw64.sh
+++ scripts/build-windows-mingw64.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 cd build
-cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
+cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DWIN=1 -DMINGW64=1
 if [ ! -f ccan_config.h ]; then
     cmake --build . --target ccan-configurator
     ./ccan-configurator.exe gcc > ccan_config.h
