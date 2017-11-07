254be7fb95c08ac294b9af637382c395f26af8b1
Linfeng Liang <lianglinfeng@gmail.com>
2017-11-07 09:53:42 +0800

    update CMakeLists.txt

---
+++ scripts/build-mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+cmake .. -DMAC=1
+cmake --build . --target ccan-configurator
+if [ ! -f ccan_config.h ]; then
+    ./ccan-configurator clang > ccan_config.h
+fi
+cmake --build . --target ln-core
+
