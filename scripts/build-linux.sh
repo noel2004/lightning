5c63d68f4d7d7aa4b39cbe0a3613b57bb02b35f6
Linfeng Liang <lianglinfeng@gmail.com>
2017-11-08 10:04:15 +0800

    u

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-OUTPUT_DIR="output/mac"
+OUTPUT_DIR="output/linux"
 rm -rf ${OUTPUT_DIR}
 mkdir -p ${OUTPUT_DIR}
 
 mkdir -p build
 cd build
-cmake .. -DLINUX=1
+cmake .. -DLINUX=1 || exit 1
 if [ ! -f ccan_config.h ]; then
     cmake --build . --target ccan-configurator
-    ./ccan-configurator clang > ccan_config.h
+    ./ccan-configurator clang > ccan_config.h || exit 1
 fi
-cmake --build . --target lncore
+cmake --build . --target lncore || exit 1
 
 cd ..
 
